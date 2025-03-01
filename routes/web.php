d389ec7e1a3733a196c42bda4230a8eb282c0417
ttnt6018 <ttnt6018@gmail.com>
2025-03-01 21:07:14 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\CourseController;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
-| 1234444
+| 123444412321321
 */
 
 Route::get('/', function () {
