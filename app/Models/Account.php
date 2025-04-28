8542ec581d0c7a181cbc8e6a6a2ba2966e008804
thanhtync123 <thanhtync123@gmail.com>
2025-04-28 18:50:32 +0700

    Đăng nhập đăng xuất

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -11,28 +11,4 @@ class Account extends Authenticatable
     use HasFactory, Notifiable;
 
     protected $guarded = [];
-
-    public function lecturer()
-    {
-        return $this->belongsTo(Lecturer::class, 'lecturer_id');
-    }
-
-    public function isAdmin()
-    {
-        return $this->role === 'Admin';
-    }
-
-    public function isLecturer()
-    {
-        return $this->role === 'Lecturer';
-    }
-
-    public function canViewLecturer($lecturerId)
-    {
-        if ($this->isAdmin()) {
-            return true;
-        }
-        
-        return $this->isLecturer() && $this->lecturer_id == $lecturerId;
-    }
 }
